78038a4a7ef4c72ad1bb01e152aace3adfa46965
Jack Yeh <jack.yeh@gmail.com>
2015-09-08 21:26:08 -0600

    add functionality: create logout: - add custom route for logout - add logout link and functionality in the view - add logout functionality in session#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get '/auth/:provider/callback', to: 'sessions#create'
-  get '/dashboard', to: 'dashboard#show'
-  get '/auth/github', as: :login
+  get     '/auth/:provider/callback',    to: 'sessions#create'
+
+  get     '/dashboard',                  to: 'dashboard#show'
+
+  delete  '/logout',        as: :logout, to: 'sessions#destroy'
+  get     '/auth/github',   as: :login
 
   root 'welcome#index'
 end
