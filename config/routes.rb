8a3c4716653924abb81b2a62ab6d217893cf6f65
Jack Yeh <jack.yeh@gmail.com>
2015-09-08 21:05:17 -0600

    add functionality: create helper method for login and corresponding custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/dashboard', to: 'dashboard#show'
+  get '/auth/github', as: :login
 
   root 'welcome#index'
 end
