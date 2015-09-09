caa96cfe2b114baea308436b435f6e6d42fef627
Jack Yeh <jack.yeh@gmail.com>
2015-09-09 15:31:02 -0600

    add mock omniauth configurations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,19 @@
 require 'simplecov'
 SimpleCov.start
 
+def github_login
+  OmniAuth.config.test_mode = true
+
+  OmniAuth.config.mock_auth[:github] = {
+    'provider'  => 'github',
+    'uid'       => '12345',
+    'token'     => 'adoifvuhadiuvh2341234',
+    'info'      => {email:      'example@go.com',
+                    nickname:   'ego',
+                    image_url:  'poijasf98u98u'}
+  }
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
