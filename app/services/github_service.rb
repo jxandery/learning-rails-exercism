7189e2e8eeb81d22f9c55b763fb64b2fced24bc5
Jack Yeh <jack.yeh@gmail.com>
2015-09-10 16:20:09 -0600

    add functionality: increase rate limit by authenticating requests

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,29 +1,30 @@
 class GithubService
-  attr_reader :connection
+  attr_reader :connection, :key, :secret
 
   def initialize
     @connection = Hurley::Client.new "https://api.github.com"
-    #connection['apikey'] = ENV['github_key']
+    @key = ENV['github_key']
+    @secret = ENV['github_secret']
   end
 
   def repos(user)
-    parse(connection.get("/users/#{user}/repos"))
+    parse(connection.get("/users/#{user}/repos?client_id=#{key}&client_secret=#{secret}"))
   end
 
   def followers(user)
-    parse(connection.get("/users/#{user}/followers"))
+    parse(connection.get("/users/#{user}/followers?client_id=#{key}&client_secret=#{secret}"))
   end
 
   def following(user)
-    parse(connection.get("/users/#{user}/following"))
+    parse(connection.get("/users/#{user}/following?client_id=#{key}&client_secret=#{secret}"))
   end
 
   def starred(user)
-    parse(connection.get("/users/#{user}/starred"))
+    parse(connection.get("/users/#{user}/starred?client_id=#{key}&client_secret=#{secret}"))
   end
 
   def orgs(user)
-    parse(connection.get("/users/#{user}/orgs"))
+    parse(connection.get("/users/#{user}/orgs?client_id=#{key}&client_secret=#{secret}"))
   end
 
   private
