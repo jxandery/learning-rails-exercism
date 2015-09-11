d3f277caeb5ef709ed45ce93e5da5480b466b6cb
Jack Yeh <jack.yeh@gmail.com>
2015-09-10 22:34:03 -0600

    add functionality: allow http connections when no cassette

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,7 @@ require 'githubstats'
 WebMock.disable_net_connect!(allow_localhost: true)
 
 VCR.configure do |config|
+  config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.default_cassette_options = { :serialize_with => :json }
