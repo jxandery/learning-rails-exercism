3c97da3fc15295b764c21e8521e8424490b92aee
Jack Yeh <jack.yeh@gmail.com>
2015-09-10 22:59:24 -0600

    create/pass test: gets commit activity for last year

--- spec/services/stats_service_spec.rb
+++ spec/services/stats_service_spec.rb
@@ -17,4 +17,12 @@ RSpec.describe 'gitservice works' do
       expect(stats.current_streak).to eq(4)
     end
   end
+
+  it 'gets commit activity' do
+    VCR.use_cassette('stats#commit_activity') do
+      stats = StatsService.new('jxandery')
+
+      expect(stats.commit_activity).to eq(1266)
+    end
+  end
 end
