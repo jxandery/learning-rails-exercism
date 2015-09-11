5cb008e9d38082430bdd4f7378a0ea2e653197e7
Jack Yeh <jack.yeh@gmail.com>
2015-09-11 07:35:48 -0600

    update test: user sees their public info

--- spec/features/user_sees_their_public_info_spec.rb
+++ spec/features/user_sees_their_public_info_spec.rb
@@ -2,12 +2,11 @@ require 'spec_helper'
 
 RSpec.describe 'User can see public info', type: :feature do
   context 'sees their followers' do
-    xit 'sees public info' do
+    it 'sees public info' do
       visit root_path
       github_login
 
-      expect(page).to have_content('Welcome ego')
-      expect(page).to have_content('9 followers')
+      expect(page).to have_content('Followers (9)')
       expect(current_path).to eq(dashboard_path)
     end
   end
