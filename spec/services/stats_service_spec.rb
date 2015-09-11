dee51c3ce2abbb576b26d6fbf8720ea0dfab9766
Jack Yeh <jack.yeh@gmail.com>
2015-09-10 22:57:45 -0600

    create/pass test: gets current streak

--- spec/services/stats_service_spec.rb
+++ spec/services/stats_service_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 RSpec.describe 'gitservice works' do
 
-  it 'gets contributions data' do
-    VCR.use_cassette('stats#contributions') do
+  it 'gets max streak' do
+    VCR.use_cassette('stats#max_streak') do
       stats = StatsService.new('jxandery')
 
       expect(stats.max_streak).to eq(20)
+    end
+  end
+
+  it 'gets current streak' do
+    VCR.use_cassette('stats#current_streak') do
+      stats = StatsService.new('jxandery')
+
+      expect(stats.current_streak).to eq(4)
     end
   end
 end
